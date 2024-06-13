31f856e9954c721ae43063056eb5d12421ab49eb
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-06-13 03:11:28 -0300

    eu tentei

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'tahiti':{
+       100: '#CE09D',
+       200: '#1C646D',
+       300: '#1FR0802',
+       400: '#38184C',
+       500: '#A0CD60',
+      },
+     
+
+    },
   },
   plugins: [],
 }
