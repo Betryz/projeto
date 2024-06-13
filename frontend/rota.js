e88eafb33b9d6192ce90c857ed71cc9106c957bf
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-06-13 20:11:18 -0300

    url pronta

--- frontend/rota.js
+++ frontend/rota.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'src')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src' , 'index.html'));
+    res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 
