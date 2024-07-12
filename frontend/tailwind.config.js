b1b3e58250a0569c85e3be9c67c3c5e432de2f23
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-07-11 22:30:30 -0300

    yes baby

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}" , './custom.css'],
   theme: {
     extend: {},
     colors: {
@@ -14,6 +14,8 @@ module.exports = {
     fontFamily: {
         'Josefin Sans' : [ 'Josefin Sans', 'sans-serif' ],
       },
+  
+
 
   
   },
